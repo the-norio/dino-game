d53104ba62b316e6024ca3656123899bd31031d9
the-norio <lucas.tnrio@gmail.com>
2023-02-01 16:43:30 -0300

    creating Spawn method that spawns an obstacle given a random chance

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -14,4 +14,36 @@ public class Spawner : MonoBehaviour
     }
 
     public SpawnableObject[] objects;
+
+    public float minSpawnRate = 1f;
+    public float maxSpawnRate = 2f;
+
+    private void OnEnable()
+    {
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void Spawn()
+    {
+        float spawnChance = Random.value;
+
+        foreach (var obj in objects)
+        {
+            if (spawnChance < obj.spawnChance)
+            {
+                GameObject obstacle = Instantiate(obj.prefab);
+                obstacle.transform.position += transform.position;
+                break;
+            }
+
+            spawnChance -= obj.spawnChance;
+        }
+
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+    }
 }
